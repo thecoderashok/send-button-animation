c1e0dd6dde127bb53bafb914e78640cf1557d70c
Ashok <cooperate.ashok@gmail.com>
2022-03-29 23:43:43 +0530

    Updated

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-let button = document.getElementById("button")
+let button = document.getElementById("button");
+let BtnIconRow = document.getElementById("button-icon-row");
 
-button.addEventListener("click", () => {
-    console.log("clicked")
-})
+function send() {
+  BtnIconRow.style.transform = "translateX(0%)";
+
+  setTimeout(() => {
+    if ((BtnIconRow.style.transform = "translateX(0%)")) {
+      document.getElementById("iconSent").style.transform = "rotate(45deg)";
+        document.getElementById("iconSent").style.opacity = "0"
+     setTimeout(() => {
+            document.getElementById("iconSent-check").style.opacity = "1"
+     }, 200);
+
+     
+      document.getElementById("button-placeholder-row").style.transform = "translateY(-50%)";
+    }
+
+
+  }, 1000);
+}
+
+
